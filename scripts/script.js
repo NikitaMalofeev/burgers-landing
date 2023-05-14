4fd1de8fff9bee4f5f3b29a4feab4a23e7436477
NikitaMalofeev <front.work3@gmail.com>
2023-05-14 22:06:17 +0300

    Добавлена валидация введенных значений, несколько переходов по структуре документа ссылками

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,72 @@
-document.getElementsByClassName("content-title")[0].style.color = "red";
 
-document.getElementById("main-actions-button").onclick = function scroll () {
-    // 1 вариант
-    // document.getElementById("products").scrollIntoView({behavior: "smooth"});
-
-    // 2 вариант
-    const test = document.getElementById("products");
-    test.scrollIntoView({behavior: "smooth"});
-    console.log(test)
-
-
+document.getElementById("main-actions-button").onclick = function () {
+    // не работает( не пойму почему, я реализовал переход по href: #products, но функция не работает, от этого нет и плавного перехода befavior, подскажите почему
+    document.getElementById("products").scrollIntoView({behavior: "smooth"});
 }
 
+let links = document.querySelectorAll(".menu__item > a");
+
+for (let i = 0; i < links.length; i++) {
+    links[i].onclick = function () {
+        document.getElementById(links[i].getAttribute("data-link")).scrollIntoView({behavior: "smooth"});
+    }
+}
+
+let buttons = document.getElementsByClassName("products__button");
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function () {
+        document.getElementById("order").scrollIntoView({behavior: "smooth"});
+    }
+}
+
+let burger = document.getElementById("burger")
+let name = document.getElementById("name")
+let phone = document.getElementById("phone")
+
+document.getElementById("order-actions").onclick = function () {
+    let hasError = false;
+
+    [burger, name, phone].forEach(item => {
+        if (!item.value) {
+            item.parentElement.style.background = "red"
+            hasError = true;
+        } else {
+            item.parentElement.style.background = ""
+        }
+    });
+
+    if (!hasError) {
+        [burger, name, phone].forEach(item => {
+            item.value = "";
+        });
+        alert('Спасибо за заказ мы скоро свяжемся с вами, а здесь будет popup)')
+        //добавить popup
+    }
+}
+
+let prices = document.getElementsByClassName("products__item-price");
+
+document.getElementById("change-currency").onclick = function (e) {
+    let currentCurrency = e.target.innerText;
+
+    let newCurrency = "$";
+    let coefficient = 1;
+
+    if(currentCurrency === "$") {
+        newCurrency = "₽";
+        coefficient = 76; //добавить курс валюты изменяющийся
+
+    } else if (currentCurrency === "₽") {
+        newCurrency = "BYN";
+        coefficient = 3;
+    } else if (currentCurrency === "BYN") {
+        currentCurrency = "$";
+        coefficient = 0.33;
+    }
+    e.target.innerText = newCurrency;
+
+    for ( let i = 0; i < prices.length; i++) {
+        prices[i].innerText = +(prices[i].getAttribute("data-base-price") * coefficient).toFixed(1) + " " + newCurrency;
+    }
+}
